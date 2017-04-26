28aca4c1a1473695437991ab9f9abd88233c6d06
Adam Szablya <szablya@gmail.com>
2017-04-26 03:42:04 +0000

    added lib for credentials

---
+++ pycam/pycam_config/creds.py
@@ -0,0 +1,20 @@
+class Cameras:
+    cam = ''
+    port = ''
+    username = ''
+    password = ''
+    image_dir = ''
+    
+    def camera_one(self):
+        self.cam = '200.79.225.83'
+        self.port = '8080'
+        self.username = ''
+        self.password = ''
+        self.image_dir = '/mjpg/video.mjpg'
+        
+        cam = self.cam
+        port = self.port
+        username = self.username
+        password = self.password
+        image_dir = self.image_dir
+        
